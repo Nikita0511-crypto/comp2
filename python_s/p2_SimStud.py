6bc5df026ca857aae2c161ab628d4bb2a1dedaac
MSXOMA <homa0286@gmail.com>
2024-11-15 16:10:51 +0200

    sim.stud

--- python_s/p2_SimStud.py
+++ python_s/p2_SimStud.py
@@ -5,21 +5,25 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 100
         self.alive = True
 
     def to_study(self):
         print("Time to study!")
         self.progress += 0.12
         self.gladness -= 3
+        self.money += 5
 
     def to_sleep(self):
         print("I will sleep!")
         self.gladness += 3
+        self.money -= 0.5
 
     def to_chill(self):
         print("Rest time!")
         self.gladness += 5
         self.progress -= 0.1
+        self.money -= 1
 
     def is_alive(self):
         if self.progress < -0.5:
@@ -35,6 +39,7 @@ class Student:
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
         print(f"Progress = {round(self.progress, 2)}")
+        print(f'Money = {self.money}')
 
     def live(self, day):
         day = "Day" + str(day) + "of" + self.name + "life"
